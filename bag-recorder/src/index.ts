01e32fd0091f75f1ae5b92326ce6d0c4a9768b09
Dhruv Tyagi <dhruv@polymathrobotics.com>
2025-08-07 16:38:22 -0700

    Bag recorder panel

---
+++ bag-recorder/src/index.ts
@@ -0,0 +1,10 @@
+import { ExtensionContext } from "@foxglove/extension";
+
+import { initBagRecorderPanel } from "./BagRecorderPanel";
+
+export function activate(extensionContext: ExtensionContext): void {
+  extensionContext.registerPanel({ 
+    name: "[Polymath] Bag Recorder", 
+    initPanel: initBagRecorderPanel 
+  });
+}
